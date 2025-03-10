b498a90be4620c404e35ec195abe916cba7d67bc
KarlaSz <szymaszkiewiczk@gmail.com>
2025-03-10 21:51:04 +0100

    files

---
+++ brainIdeas/ideas/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+# #model to jak 1 tabela w bazie danych dlatego sa w liczbie pojedynczej
+
+IDEA_STATUS = (
+    ('pending', 'Waiting for review'),
+    ('accepted', 'Accepted'),
+    ('done', 'Done'),
+    ('rejected', 'Rejected')
+)
+
+class Idea(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    youtube_url = models.URLField(null=True, blank=True)
+    status = models.CharField(choices=IDEA_STATUS, max_length=30, default='pending')
+
+# pomysl posiada wiele glosow - Foreign key
+class Vote(models.Model):
+    idea = models.ForeignKey(Idea, on_delete=models.CASCADE)
+    reason = models.TextField()
+
